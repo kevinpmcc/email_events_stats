9360f6df66f3478646c177f07b3664c8b612bd3c
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-08-17 11:37:25 +0100

    created failing feature test to show 1 sent email, next step create Email model

--- spec/features/emails_feature_spec.rb
+++ spec/features/emails_feature_spec.rb
@@ -7,4 +7,19 @@ feature 'emails' do
       expect(page).to have_content 'Email Statistics'
     end
   end
+
+  context 'one email sent event data received' do
+    before :each do
+      Email.create(address: 'barney@lostmy.name',
+                   email_type: 'Shipment',
+                   event: 'send',
+                   timestamp: '1471429434')
+    end 
+
+    scenario 'it should display total number of emails sent' do
+      visit '/emails'
+      emails_sent = page.find("#emails-sent") 
+      expect(emails_sent).to have_content '1'
+    end
+  end
 end
