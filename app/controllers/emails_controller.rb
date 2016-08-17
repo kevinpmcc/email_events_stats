36e6f18a9c4d1225a0d343ab05c161030eb6c6a2
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-08-17 13:41:43 +0100

    json post test passing and working in development with llirdnam master sample jsons

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,23 @@
 class EmailsController < ApplicationController
 
+  skip_filter :verify_authenticity_token
+
   def index
+  end
+
+  def create
+    email = Email.new(email_params)
+    render json: email if email.save
+  end
+
+  def email_params
+    data = JSON.load(request.body) 
+    {
+      address: data['Address'],
+      email_type: data['EmailType'],
+      event: data['Event'],
+      timestamp: data['Timestamp']
+    }
   end
 
 end
--- spec/requests/emails_json_spec.rb
+++ spec/requests/emails_json_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Emails", :type => :request do
     headers = { "CONTENT_TYPE" => "application/json" }
     post "/emails", '{"Address":"barney@lostmy.name","EmailType":"Shipment","Event":"send","Timestamp":1471435479}', headers
     expect(response.status).to eq(200)
+    first_email = Email.first
+    expect(first_email.address).to eq('barney@lostmy.name')
+    expect(first_email.email_type).to eq('Shipment')
+    expect(first_email.event).to eq('send')
   end
 
 end
