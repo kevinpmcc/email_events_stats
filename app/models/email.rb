208e5b70715ec75be31ff95b1a68bb872f459069
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-08-19 13:02:37 +0100

    adding the model file missed in last commit

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,15 +1,13 @@
 class Email < ActiveRecord::Base
 
-  def total_sent
-    Email.where(event: 'send').count
+  def self.event_total(event)
+    Email.where(event: event).count
   end  
 
-  def total_opened
-    Email.where(event: 'open').count
+  def self.event_rate_by_type(email_type: ,event:)
+    emails = Email.all
+    sent_emails = emails.where(event: 'send', email_type: email_type).count
+    emails_with_event = emails.where(event: event, email_type: email_type).count
+    (emails_with_event.to_f / sent_emails).round(2)
   end
-
-  def total_clicked
-    Email.where(event: 'click').count
-  end
-
 end
